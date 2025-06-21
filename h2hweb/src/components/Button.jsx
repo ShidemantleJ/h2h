9229edd874c9f37242c197f49eb73ded7a260f51
ShidemantleJ <jcshidemantle@crimson.ua.edu>
2025-06-21 11:18:46 -0500

    add sitemap, modify url meta property

--- h2hweb/src/components/Button.jsx
+++ h2hweb/src/components/Button.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function Button({ text, color = "green", onClick, type = "button" }) {
   return (
     <button
-      className={`${color === "green" && "bg-emerald-700"} ${
-        color === "red" && "bg-red-900"
-      } cursor-pointer mx-auto inline my-auto text-sm px-4 py-1.5 rounded-lg font-semibold`}
+      className={`${color === "green" && "bg-emerald-700 hover:bg-emerald-800"} ${
+        color === "red" && "bg-red-900 hover:bg-red-950"
+      } transition-all duration-400 cursor-pointer mx-auto inline my-auto text-sm px-4 py-1.5 rounded-lg font-semibold`}
       onClick={onClick}
       type={type}
     >
--- h2hweb/src/pages/Home.jsx
+++ h2hweb/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { Users } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
 
 const Home = (props) => {
   const navigate = useNavigate();
@@ -7,10 +8,12 @@ const Home = (props) => {
     <div className="h-screen w-full p-12 overflow-hidden font-sans space-y-8">
       <div className="text-center">
         <h2 className="text-white font-bold text-6xl">H2HCube</h2>
-        <h2 className="text-emerald-600 mt-2 text-3xl">Compete Head-to-Head with Other Cubers</h2>
+        <h2 className="text-emerald-600 mt-2 text-3xl">
+          Compete Head-to-Head with Other Cubers
+        </h2>
       </div>
       <div
-        onClick={() => (navigate('/play'))}
+        onClick={() => navigate("/play")}
         className="mx-auto cursor-pointer bg-emerald-700 flex p-5 w-70 items-center rounded-lg hover:bg-emerald-800 transition-all"
       >
         <Users className="inline-block mr-4" />
@@ -18,6 +21,16 @@ const Home = (props) => {
           <p className="font-bold text-md">Play Online</p>
           <p className="text-zinc-200 text-sm">Challenge a friend</p>
         </div>
+      </div>
+      <div className="absolute bottom-0 right-0 m-5">
+        <a
+          href="https://github.com/ShidemantleJ/h2h"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-block text-zinc-300 hover:text-emerald-400 underline text-md transition-colors"
+        >
+          View on GitHub
+        </a>
       </div>
     </div>
   );
